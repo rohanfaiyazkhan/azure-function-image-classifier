1c6f821bd54fbce9b6e54cd460ed6fd9ce58de4c
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2021-06-18 00:16:48 +0600

    Modified function to use file from request body

--- start/classify/__init__.py
+++ start/classify/__init__.py
@@ -4,18 +4,31 @@ import azure.functions as func
 import json
 
 # Import helper script
-from .predictonnx import predict_image_from_url
+from .predictonnx import predict_image_from_file
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    image_url = req.params.get('img') or req.get_body()
-    logging.info('Image URL received: ' + image_url)
+    file = None
 
-    results = predict_image_from_url(image_url)
+    try:
+        file = req.files['file']
+        logging.info(file)
+    except:
+        logging.error('File not found')
 
     headers = {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*"
     }
 
-    return func.HttpResponse(json.dumps(results, indent=4), headers=headers)
+    if file:
+        logging.info('Image file received: ' + file.filename)
+
+        results = predict_image_from_file(file)
+
+        return func.HttpResponse(json.dumps(results, indent=4), headers=headers)
+    else:
+        return func.HttpResponse(
+            "Please pass a file in the request",
+            headers=headers, status_code=400
+        )
