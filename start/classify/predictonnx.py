79a5d36265a149f045f377199609bda116a30bb5
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-19 12:39:30 -0400

    Emotion detection rework

--- start/classify/predictonnx.py
+++ start/classify/predictonnx.py
@@ -1,3 +1,4 @@
+import enum
 import numpy as np    # we're going to use numpy to process input and output data
 import onnxruntime    # to inference ONNX models, we use the ONNX Runtime
 import onnx
@@ -13,6 +14,7 @@ from datetime import datetime
 
 # display images in notebook
 from PIL import Image, ImageDraw, ImageFont, ImageOps
+import cv2
 
 
 def load_labels(path):
@@ -23,7 +25,7 @@ def load_labels(path):
 
 # Run the model on the backend
 d = os.path.dirname(os.path.abspath(__file__))
-modelfile = os.path.join(d, 'model.onnx')
+modelfile = os.path.join(d, 'detect_emotion.onnx')
 labelfile = os.path.join(d, 'labels.json')
 
 session = onnxruntime.InferenceSession(modelfile, None)
@@ -35,20 +37,19 @@ labels = load_labels(labelfile)
 
 
 def preprocess(input_data):
-    # convert the input data into the float32 input
-    img_data = input_data.astype('float32')
+    img_data = resize_image_to_square(input_data)
+
+    print(img_data.shape)
 
     # normalize
     mean_vec = np.array([0.485, 0.456, 0.406])
     stddev_vec = np.array([0.229, 0.224, 0.225])
-    norm_img_data = np.zeros(img_data.shape).astype('float32')
-    for i in range(img_data.shape[0]):
-        norm_img_data[i, :, :] = (
-            img_data[i, :, :]/255 - mean_vec[i]) / stddev_vec[i]
+    norm_img_data = img_data/255
 
     # add batch channel
-    norm_img_data = norm_img_data.reshape(1, 3, 224, 224).astype('float32')
-    return norm_img_data
+    img_data = norm_img_data.reshape(1, 1, 48, 48).astype('float32')
+
+    return img_data
 
 
 def softmax(x):
@@ -59,6 +60,50 @@ def softmax(x):
 
 def postprocess(result):
     return softmax(np.array(result)).tolist()
+
+
+def resize_image_to_square(im: np.ndarray, desired_size=48):
+    im = Image.fromarray(im)
+    old_size = im.size
+    ratio = float(desired_size)/min(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+
+    im = im.resize(new_size, Image.Resampling.LANCZOS)
+    new_im = Image.new("L", (desired_size, desired_size))
+    new_im.paste(im, ((desired_size-new_size[0])//2,
+                      (desired_size-new_size[1])//2))
+
+    return np.array(new_im)
+
+
+def crop_faces(im: Image, padding=8):
+    im = im.convert("L")
+    im_arr = np.array(im)
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(
+        im_arr, scaleFactor=1.25, minNeighbors=3, minSize=(40, 40))
+
+    print('Number of faces detected:', len(faces))
+
+    crops = []
+
+    for count, face in enumerate(faces):
+        x, y, w, h = face
+
+        left = x - padding
+        right = x + w + padding
+        top = y - padding
+        bottom = y + h + padding
+        crop = im_arr[left:right, top:bottom]
+
+        crops.append({'img': crop, 'weight': x+y})
+
+    # ensure that faces are sorted in the order in which they appear
+    crops.sort(key=lambda x: x['weight'])
+    crops = [c['img'] for c in crops]
+
+    return faces, crops
 
 
 def predict_image_from_file(file):
@@ -86,28 +131,44 @@ def predict_image_from_file(file):
         return response
 
 
+def run_single_inference(raw_input):
+    input_data = preprocess(raw_input)
+
+    raw_result = session.run([], {input_name: input_data})
+
+    res = postprocess(raw_result)
+
+    idx = np.argmax(res)
+
+    label = labels[idx]
+    conf = res[idx]
+
+    return label, conf, res
+
+
 def predict_image_from_url(image_url):
     with urlopen(image_url) as testImage:
         image = Image.open(testImage)
 
     imnew = ImageOps.fit(image, (224, 224))
 
-    image_data = np.array(imnew).transpose(2, 0, 1)
-    input_data = preprocess(image_data)
+    faces, crops = crop_faces(imnew)
+
+    results = []
 
     start = time.time()
-    raw_result = session.run([], {input_name: input_data})
-    end = time.time()
-    res = postprocess(raw_result)
+    for crop in crops:
+        singe_result = run_single_inference(crop)
+        results.append(singe_result)
 
+    end = time.time()
     inference_time = np.round((end - start) * 1000, 2)
-    idx = np.argmax(res)
 
     response = {
         'created': datetime.utcnow().isoformat(),
-        'prediction': labels[idx],
+        'results': results,
         'latency': inference_time,
-        'confidence': res[idx]
+        'faces': faces
     }
     logging.info(f'returning {response}')
     return response
